f87874b0e825654091bf99763fc0fa09e31570d0
Luiz_Borges <88781739+luizborges17@users.noreply.github.com>
2024-11-21 23:18:33 -0300

    Delete lp2Nadalete directory

--- lp2Nadalete/src/main/java/org/example/repositorys/DispositivosInformaticaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repositorys;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
-import org.example.models.DispositivoInformatica;
-
-@Repository
-public interface DispositivosInformaticaRepository extends JpaRepository<DispositivoInformatica, Integer> {
-}
